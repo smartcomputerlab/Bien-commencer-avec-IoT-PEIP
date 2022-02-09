74d0863e5815100fbecc1f81c7ad01cb47f3502a
bako <przemyslaw.bakowski@univ-nantes.fr>
2022-02-09 10:13:39 +0100

    Lab1

---
+++ x.i2c.scan.py
@@ -0,0 +1,11 @@
+import machine
+i2c = machine.I2C(scl=machine.Pin(14), sda=machine.Pin(12))
+print('Scan i2c bus...')
+devices = i2c.scan()
+if len(devices) == 0:
+    print("No i2c device !")
+else:
+    print('i2c devices found:',len(devices))
+for device in devices:
+    print("Decimal address: ",device," | Hexa address: ",hex(device))
+
